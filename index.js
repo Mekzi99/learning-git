6217a6d2aaaaa7509978c2f5dfc0d86436c07118
Emeka Mbaebie <mbaebieemeka@gmail.com>
2022-08-18 23:22:50 +0100

    index.js modified

--- index.js
+++ index.js
@@ -5,7 +5,8 @@
 </head>
 <body>
   <h1>This is a Heading</h1>
-  <p>This is a Paragraph</p>
-  <p>This is another Paragraph</p>
+  <p>First paragraph.</p>
+  <p>This is a paragraph.</p>
+  <p>This is another paragraph.</p>
 </body>
 </html>
