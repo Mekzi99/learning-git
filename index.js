31f84d3f44a83078e7c352a4ec4c91d37ebae937
Emeka Mbaebie <mbaebieemeka@gmail.com>
2022-08-18 22:57:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,8 @@
 <title>Page Title</title>
 </head>
 <body>
-  <p>This is another Paragraph</p>
   <h1>This is a Heading</h1>
   <p>This is a Paragraph</p>
+  <p>This is another Paragraph</p>
 </body>
 </html>
- 
